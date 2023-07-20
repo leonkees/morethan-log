853e75ff25b3c8c58c380fbd4f2b841567895955
leonkees <leon_f_k@hotmail.com>
2023-07-20 12:13:43 +0200

    update favicon y avatar

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Ozom Kit",
-    image: "/avatar.jfif", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "logo-gradient.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "by Ozom Builders",
     bio: "Empowering & Inspiring",
     email: "ozombuilders@gmail.com",
